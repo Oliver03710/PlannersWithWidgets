a1acf5c66fa68493c67a2e62710afc44872a4d8f
Oliver03710 <jhee2619@gmail.com>
2023-01-19 17:52:30 +0900

    Create: Base Components(ViewController & View) 생성

---
+++ PlannersWithWidgets/Utilities/BaseComponents/ViewControllers/BaseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseViewController.swift
+//  PlannersWithWidgets
+//
+//  Created by Junhee Yoon on 2023/01/19.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Functions
+    
+}
---
+++ PlannersWithWidgets/Utilities/BaseComponents/Views/BaseView.swift
@@ -0,0 +1,20 @@
+//
+//  BaseView.swift
+//  PlannersWithWidgets
+//
+//  Created by Junhee Yoon on 2023/01/19.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
