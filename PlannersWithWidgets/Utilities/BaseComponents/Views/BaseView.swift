3681266a7b672af264301d7fc2286a88986e7576
Oliver03710 <jhee2619@gmail.com>
2023-01-19 18:04:43 +0900

    style: BaseView 기본 구성

--- PlannersWithWidgets/Utilities/BaseComponents/Views/BaseView.swift
+++ PlannersWithWidgets/Utilities/BaseComponents/Views/BaseView.swift
@@ -8,13 +8,21 @@
 import UIKit
 
 class BaseView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        configureUI()
+        setConstraints()
     }
-    */
-
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    func configureUI() { }
+    func setConstraints() { }
 }
