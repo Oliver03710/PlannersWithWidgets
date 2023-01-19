f104ea9421481b85b580e54a7f29dd5cab49a822
Oliver03710 <jhee2619@gmail.com>
2023-01-19 18:25:35 +0900

    style: Indentation 수정

--- PlannersWithWidgets/Utilities/Extensions/Preview+Extension.swift
+++ PlannersWithWidgets/Utilities/Extensions/Preview+Extension.swift
@@ -10,18 +10,18 @@ import SwiftUI
 #if DEBUG
 extension UIViewController {
     private struct Preview: UIViewControllerRepresentable {
-            let viewController: UIViewController
-
-            func makeUIViewController(context: Context) -> UIViewController {
-                return viewController
-            }
-
-            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-            }
+        let viewController: UIViewController
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
         }
-
-        func toPreview() -> some View {
-            Preview(viewController: self)
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         }
+    }
+    
+    func toPreview() -> some View {
+        Preview(viewController: self)
+    }
 }
 #endif
