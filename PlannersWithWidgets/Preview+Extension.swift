9f0b21d93c993b371c6d13f2487da29bd463aed1
Oliver03710 <jhee2619@gmail.com>
2023-01-19 16:08:36 +0900

    Create: Extension에 Preview 기능 추가

---
+++ PlannersWithWidgets/Preview+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  Preview+Extension.swift
+//  PlannersWithWidgets
+//
+//  Created by Junhee Yoon on 2023/01/19.
+//
+
+import SwiftUI
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+            let viewController: UIViewController
+
+            func makeUIViewController(context: Context) -> UIViewController {
+                return viewController
+            }
+
+            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            }
+        }
+
+        func toPreview() -> some View {
+            Preview(viewController: self)
+        }
+}
+#endif
--- PlannersWithWidgets/ViewController.swift
+++ PlannersWithWidgets/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
@@ -13,34 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .yellow
     }
-
-
 }
 
-#if DEBUG
-import SwiftUI
-
-struct ViewControllerRepresentable: UIViewControllerRepresentable {
-    
-func updateUIViewController(_ uiView: UIViewController, context: Context) {
-    
-}
-    
-@available(iOS 13.0.0, *)
-func makeUIViewController(context: Context) -> UIViewController {
-    ViewController()
-    }
-}
-
-@available(iOS 13.0, *)
-struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
+struct ViewControllerPreview: PreviewProvider {
     static var previews: some View {
-        Group {
-            ViewControllerRepresentable()
-                .ignoresSafeArea()
-                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 14 pro"))
-        }
-        
+        ViewController().toPreview()
+            .ignoresSafeArea()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 14 pro"))
     }
-} #endif
+}
